4da5eb05b8abf82982e59bb1b853cedaf0f87013
Franco Adorno <fradorno@uade.edu.ar>
2026-08-12 14:22:22 -0300

    git commit -m "suma de numeros"

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+print("Calculadora en Python") 
+
+# Inicializar variables 
+numero1 = 4 
+numero2 = 6 
+
+# Sumar los números 
+suma = numero1 + numero2 
+
+# Mostrar el resultado 
+print(f"El resultado de la suma es: {suma}") 
